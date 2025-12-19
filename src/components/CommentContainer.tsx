f928ad64873217ad3b0fe97b2b4fa96e263a146b
Tae.Garam <drasla@naver.com>
2025-12-19 12:02:41 +0900

    CommentContainer 댓글 출력 기능 완료

--- src/components/CommentContainer.tsx
+++ src/components/CommentContainer.tsx
@@ -2,9 +2,10 @@ import type { User } from "firebase/auth";
 import styled from "styled-components";
 import { ActionButton } from "../styles/styles.tsx";
 import { useForm } from "react-hook-form";
-import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { addDoc, collection, getDocs, orderBy, query, Timestamp } from "firebase/firestore";
 import {db} from "../firebase.ts";
 import {useNavigate} from "react-router";
+import { useCallback, useEffect, useState } from "react";
 
 type Props = {
     postId: string;
@@ -55,12 +56,56 @@ const ErrorMessage = styled.p`
     font-size: 14px;
 `;
 
+const CommentList = styled.ul`
+    list-style: none;
+`;
+
+const CommentItem = styled.li`
+    padding: 15px 0;
+    border-bottom: 1px solid #f1f1f1;
+`;
+
+const Meta = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 5px;
+    font-size: 13px;
+    color: #888;
+    
+    strong {
+        color: #333;
+        margin-right: 10px;
+    }
+`;
+
+const DeleteButton = styled.button`
+    background: none;
+    border: none;
+    color: tomato;
+    font-size: 12px;
+    cursor: pointer;
+    text-decoration: underline;
+    
+    &:hover {
+        color: #a71d2a;
+    }
+`;
+
 type CommentFormData = {
     content: string;
 };
 
+type CommentType = {
+    id: string,
+    content: string,
+    createdAt: Timestamp,
+    userId: string,
+    username: string,
+}
+
 function CommentContainer({ postId, currentUser}: Props) {
     const navigate = useNavigate();
+    const [comments, setComments] = useState<CommentType[]>([]);
 
     // 1-1. 댓글을 작성하는 폼 만들기
     // 1-2. 폼데이터(textarea)를 submit할 때 firebase에 쓰는 핸들러(function) 만들기
@@ -91,8 +136,7 @@ function CommentContainer({ postId, currentUser}: Props) {
             )
             reset();   // 폼에 입력되어져 있는 값 초기화
 
-            // 임시로 화면을 갱신하여 등록한 코멘트가 나오는지 확인
-            navigate(0);
+            await fetchComments();
         } catch (e) {
             alert("댓글을 작성하지 못 했습니다.");
             console.log(e);
@@ -101,9 +145,45 @@ function CommentContainer({ postId, currentUser}: Props) {
 
     // 2-1. 댓글의 내용을 불러오기
     // 2-2. 댓글의 내용을 화면에 출력해주기
+    const fetchComments = useCallback(
+        async () => {
+            try {
+                // 데이터를 불러오고
+                const querySnapshot = query(
+                    collection(db, "posts", postId, "comments"),
+                    orderBy("createdAt", "desc")
+                );
+                const snapshot = await getDocs(querySnapshot);
+
+                // 데이터를 가공하고
+                const loadedComments = snapshot.docs.map(
+                    (item) => {
+                        const data = item.data();
+                        return {
+                            id: item.id,
+                            content: data.content,
+                            createdAt: data.createdAt,
+                            userId: data.userId,
+                            username: data.username,
+                        }
+                    }
+                )
+
+                // 데이터를 state에 저장
+                setComments(loadedComments);
+            } catch (e) {
+                console.log(e);
+            }
+        },
+        [postId]
+    )
 
     // 3-1. 댓글의 목록 중, 본인이 작성한 댓글에는 삭제 버튼 출력해주기
     // 3-2. 삭제 버튼 누르면 삭제하기
+
+    useEffect(() => {
+        fetchComments();
+    }, [fetchComments]);
 
     return (
         <Section>
@@ -124,6 +204,21 @@ function CommentContainer({ postId, currentUser}: Props) {
                 </InputWrapper>
                 {errors.content && <ErrorMessage>{errors.content.message}</ErrorMessage>}
             </Form>
+
+            <CommentList>
+                {comments.map((item, index) => (
+                    <CommentItem key={index}>
+                        <Meta>
+                            <div>
+                                <strong>{item.username.split("@")[0]}</strong>
+                                <span>{item.createdAt.toDate().toLocaleDateString()}</span>
+                            </div>
+                            <DeleteButton>삭제</DeleteButton>
+                        </Meta>
+                        <div>{item.content}</div>
+                    </CommentItem>
+                ))}
+            </CommentList>
         </Section>
     );
 }
